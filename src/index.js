8da9e27641cd70818cee6145bcf57102d61404ce
armandoSannin <armandosannin@gmail.com>
2024-03-19 22:37:35 -0600

    Modificado index.js para eliminar verify token

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import app from "./app.js";
+export default app;
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import {verifyToken} from "./middlewares/authJwt.js";
-export {verifyToken};
 dotenv.config();
-// app.listen(3000);
+
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("Connected to MongoDB Atlas");
 }).catch((error) => {
     console.error(error);
 })
-// console.log("Listening on port ",3000);
+
+
